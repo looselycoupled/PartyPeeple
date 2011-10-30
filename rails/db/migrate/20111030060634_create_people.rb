0e6d68d41857092cc24505c86416df94572aadd5
Allen Leis <allen.leis@threespot.com>
2011-10-30 02:07:22 -0400

    adds initial person model

---
+++ rails/db/migrate/20111030060634_create_people.rb
@@ -0,0 +1,16 @@
+class CreatePeople < ActiveRecord::Migration
+  def self.up
+    create_table :people do |t|
+      t.string :name
+      t.string :identifier
+      t.string :access_token
+      t.string :email
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :people
+  end
+end

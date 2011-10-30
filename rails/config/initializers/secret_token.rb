1d602505fae22e363d559bed8492fd2fe5fa9c3f
Allen Leis <allen.leis@threespot.com>
2011-10-30 02:02:05 -0400

    bare rails site

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partypeeple::Application.config.secret_token = '6c067d22cb863982bc66fb8a0e29b8970db218d10a5af1ec2c85abb82499eac29a246dbca0b3792f06530892b8fe999269b0b308f8d1eb3bd5e5dedaa8a4a11a'

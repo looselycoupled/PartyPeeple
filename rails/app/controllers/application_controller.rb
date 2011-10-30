247bd519c86ad3918edc8a228877feca49641a1e
Allen Leis <allen.leis@threespot.com>
2011-10-30 03:55:54 -0400

    adds production value fb config

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -2,12 +2,18 @@ class ApplicationController < ActionController::Base
   before_filter :setup_fb_graph, :load_person
   protect_from_forgery
 
-
   private 
   
   def setup_fb_graph
-    @app_id = "133107960128280"
-    @app_secret = "f751d36bbfe6e9dd983f2516c040baaf"
+    case Rails.env
+      when "development"
+        @app_id = "133107960128280"
+        @app_secret = "f751d36bbfe6e9dd983f2516c040baaf"
+      when "production"
+        @app_id = "269316813112867"
+        @app_secret = "e8724d598f1ea7e49254f0f80a03c7d2"
+      end
+
     @app_perms = "email"
   end
   

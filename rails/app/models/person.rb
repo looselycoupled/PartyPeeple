f74f1028f7bd2c12de0e2f5c2e391d16032ebe5b
Allen Leis <allen.leis@threespot.com>
2011-11-03 00:37:17 -0400

    completes addition of likes as pages and associates with person; changes delayed job call

--- rails/app/models/person.rb
+++ rails/app/models/person.rb
@@ -4,10 +4,12 @@ class Person < ActiveRecord::Base
   def fetch_facebook_data
     fb = FbGraph::User.me(access_token).fetch
     
+    # save likes as pages
     likes = fb.likes
-
-
-
+    likes.each do |page|
+      p = Page.find_or_initialize_by_identifier(:identifier => page.identifier, :name => page.name, :category => page.category)
+      pages << p unless pages.include? p
+    end
 
   end
 

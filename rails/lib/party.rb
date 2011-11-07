163eedba1dbbbfa3512c8d067958928f47fa26b2
Allen Leis <allen.leis@threespot.com>
2011-11-06 20:05:17 -0500

    fixes incorrect evaluation

--- rails/lib/party.rb
+++ rails/lib/party.rb
@@ -6,7 +6,7 @@ module Party
     # IF they are a match for sexual interest  (from your perspective) and they have Digital Capital equal to or greater than [your Digital Capital - 50], THEN…  Flirt
     # IF they are a match for a for sexual interest  (from your perspective) and they have Digital Capital less than [your Digital Capital - 50], THEN… Fuck
     
-    if (person.preference == "both" || person.preference = target.gender)
+    if (person.preference == "both" || person.preference == target.gender)
       if ((target.digital_capital || 0) >= (person.digital_capital || 0) - 50)
         "flirt"
       else

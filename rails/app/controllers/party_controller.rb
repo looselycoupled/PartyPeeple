b3964d51f742ba451b2552ca5527c1c9ad5131b7
Allen Leis <allen.leis@threespot.com>
2011-11-08 22:36:47 -0500

    adds conversation starter icons; new conversation starters

--- rails/app/controllers/party_controller.rb
+++ rails/app/controllers/party_controller.rb
@@ -21,6 +21,7 @@ class PartyController < ApplicationController
   # display a person's details
   def peep
     @peep = Person.find_by_identifier(params[:identifier])
+    @friends_in_common = Party::friends_in_common(@person, @peep)
   end
 
   # save orientation/preference choice

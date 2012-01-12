11ac9d987978ce1b96ca523a4ae013dedae3e6a4
Allen Leis <allen.leis@threespot.com>
2012-01-11 20:08:56 -0500

    changed default route

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -86,8 +86,9 @@ Partypeeple::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "public#closed"
-
+  # root :to => "public#closed"
+  root :to => "public#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
